10ce96bb9d6d7c8f8747e39be8c49ee53193673e
FLIGHEN <49404527+FLIGHEN@users.noreply.github.com>
2021-10-31 14:59:41 +0700

    Money amount logic

--- Assets/Scripts/Player/MoneyPicker.cs
+++ Assets/Scripts/Player/MoneyPicker.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class MoneyPicker : MonoBehaviour
 {
     private string moneyTag = "Money";
+    public uint moneyAmount; 
     // Start is called before the first frame update
     public void OnTriggerEnter2D(Collider2D  other)
     {
         if (other.CompareTag(moneyTag))
+        {
+            moneyAmount += other.gameObject.GetComponent<Money>().amount;
             Destroy(other.gameObject);
+        }
     }
 }
