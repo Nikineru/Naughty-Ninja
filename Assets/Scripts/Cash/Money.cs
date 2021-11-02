6cef1d5e6c91150935f869e97d8da6d7a7df09f2
Nikineru <55121362+Nikineru@users.noreply.github.com>
2021-11-02 18:52:12 +0700

    #New money generation

--- Assets/Scripts/Cash/Money.cs
+++ Assets/Scripts/Cash/Money.cs
@@ -1,8 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Money : MonoBehaviour
 {
     public byte amount;
+
+    public float Height { get => height; }
+    [SerializeField] private float height;
+
+    [SerializeField] private Transform TopCoin;
+    [SerializeField] private Transform BottomCoin;
+
+    private void OnValidate()
+    {
+        if (TopCoin == null || BottomCoin == null)
+            return;
+
+        height = (TopCoin.localPosition.y - BottomCoin.localPosition.y) + 1 * TopCoin.localScale.y;
+    }
 }
--- Assets/Scripts/Cash/MoneyGenerator.cs
+++ Assets/Scripts/Cash/MoneyGenerator.cs
@@ -1,22 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoneyGenerator : MonoBehaviour
 {
-    public Transform pos1;
-    public Transform pos2;
+    [SerializeField] private float spawn_range;
+    [SerializeField] private float spawn_delay;
+    [SerializeField] private Money coin_prefab;
+    private Camera _camera;
 
-    public Transform moneyPrefab;
-
-    private void Start()
+    private void Awake()
     {
-        float position_X = Random.Range(pos1.position.x, pos2.position.x);
-        float position_Y = Random.Range(pos2.position.y, pos1.position.y);
+        _camera = Camera.main;
+        InvokeRepeating(nameof(SpawnCoins), 0, spawn_delay);
+    }
 
-        Vector2 MoneyPos = new Vector2(position_X, position_Y);
+    private void SpawnCoins()
+    {
+        Vector2 right_borded_position = _camera.ViewportToWorldPoint(new Vector2(1, 0.5f));
+        float spawn_shift = spawn_range / 2;
 
-        Transform Cash = Instantiate(moneyPrefab, MoneyPos, Quaternion.identity);
-        Cash.parent = transform;
+        RaycastHit2D top_hit = Physics2D.Raycast(right_borded_position, Vector2.up, distance: spawn_shift);
+        RaycastHit2D bottom_hit = Physics2D.Raycast(right_borded_position, Vector2.down, distance: spawn_shift);
+
+        float max_height = top_hit.transform == null ? right_borded_position.y + spawn_shift : top_hit.point.y;
+        float min_height = bottom_hit.transform == null ? right_borded_position.y - spawn_shift : bottom_hit.point.y;
+        print(max_height);
+
+        max_height -= coin_prefab.Height / 2;
+        min_height += coin_prefab.Height / 2;
+
+        float curret_height = Random.Range(max_height, min_height);
+        Instantiate(coin_prefab, new Vector2(right_borded_position.x, max_height), Quaternion.identity);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_camera == null)
+            return;
+
+        Vector2 right_borded_position = _camera.ViewportToWorldPoint(new Vector2(1, 0.5f));
+        float spawn_shift = spawn_range / 2;
+
+        RaycastHit2D top_hit = Physics2D.Raycast(right_borded_position, Vector2.up, distance: spawn_shift);
+        RaycastHit2D bottom_hit = Physics2D.Raycast(right_borded_position, Vector2.down, distance: spawn_shift);
+
+        float max_height = top_hit.transform == null ? right_borded_position.y + spawn_shift : top_hit.point.y;
+        float min_height = bottom_hit.transform == null ? right_borded_position.y - spawn_shift : bottom_hit.point.y;
+        float curret_height = Random.Range(max_height, min_height);
+
+        Gizmos.DrawSphere(new Vector2(right_borded_position.x, min_height), 1);
+        Gizmos.DrawSphere(new Vector2(right_borded_position.x, max_height), 1);
     }
 }
